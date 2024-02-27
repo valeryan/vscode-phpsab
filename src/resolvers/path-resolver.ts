115cf782d69bd2eb63ad256640cc5d07f5502b45
Samuel Hilson <samrhilson@gmail.com>
2024-02-27 12:05:56 -0600

    fix and simplify checks for reslovedPath

--- src/resolvers/path-resolver.ts
+++ src/resolvers/path-resolver.ts
@@ -12,7 +12,7 @@ interface PathResolverOptions {
 }
 
 const resolvePath = async (resolvers: PathResolver[]): Promise<string> => {
-  let resolvedPath: string | null = null;
+  let resolvedPath: string = '';
   for (const resolver of resolvers) {
     const resolverPath = await resolver.resolve();
     if (resolverPath) {
@@ -32,7 +32,7 @@ export const createPathResolver = (
 ): PathResolver => {
   const executableFile = executable + getPlatformExtension();
   const resolvers: PathResolver[] = [];
-  if (options.workspaceRoot !== null) {
+  if (options.workspaceRoot) {
     resolvers.push(
       createComposerPathResolver(
         executableFile,
