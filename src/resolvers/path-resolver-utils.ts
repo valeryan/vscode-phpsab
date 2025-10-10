5993f33e3c4b3606f9e8909bce4533d8a48ce694
yCodeTech <studocwho@hotmail.com>
2025-10-10 03:30:52 +0100

    refactor: extract platform check into new dedicated `isWin` function.
    - Extracted the `process.platform` tests from all the path resolver util functions into a new dedicated `isWin` util function. This makes the code more readable, and we can use the new util function in other files too where needed.

--- src/resolvers/path-resolver-utils.ts
+++ src/resolvers/path-resolver-utils.ts
@@ -1,13 +1,16 @@
 import path from 'node:path';
 
-export const getPlatformExtension = (): string =>
-  /^win/.test(process.platform) ? '.bat' : '';
+/**
+ * Is the current platform Windows?
+ * @returns boolean
+ */
+export const isWin = (): boolean => /^win/.test(process.platform);
 
-export const getPlatformPathSeparator = (): string =>
-  /^win/.test(process.platform) ? '\\' : '/';
+export const getPlatformExtension = (): string => (isWin() ? '.bat' : '');
 
-export const getEnvPathSeparator = (): string =>
-  /^win/.test(process.platform) ? ';' : ':';
+export const getPlatformPathSeparator = (): string => (isWin() ? '\\' : '/');
+
+export const getEnvPathSeparator = (): string => (isWin() ? ';' : ':');
 
 // Create passthrough methods for path, allows for easier replacement in test
 export const joinPaths = (...args: string[]): string => path.join(...args);
