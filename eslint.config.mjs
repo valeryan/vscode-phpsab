189171b2501de4837d05f283603375b8e4502c90
yCodeTech <studocwho@hotmail.com>
2026-08-29 04:39:35 +0100

    style: auto formatting of eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,27 +10,27 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([
-    globalIgnores(['**/out', '**/dist', '**/*.d.ts']),
-    {
-        extends: compat.extends('plugin:prettier/recommended'),
+  globalIgnores(['**/out', '**/dist', '**/*.d.ts']),
+  {
+    extends: compat.extends('plugin:prettier/recommended'),
 
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-            prettier,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 6,
-            sourceType: 'module',
-        },
-
-        rules: {},
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
     },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 6,
+      sourceType: 'module',
+    },
+
+    rules: {},
+  },
 ]);
