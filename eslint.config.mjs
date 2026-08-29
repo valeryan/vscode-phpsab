dabe56f8937a18bce925d62f4f162e235a4ead91
yCodeTech <studocwho@hotmail.com>
2026-08-29 05:54:49 +0100

    chore(eslint): ensure `.prettierrc` file is being used in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,14 @@ export default defineConfig([
       sourceType: 'module',
     },
 
-    rules: {},
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
+    },
   },
 ]);
