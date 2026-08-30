685b80fe89c1dcb5d587128e20adfdeca0d2c8d8
yCodeTech <studocwho@hotmail.com>
2026-08-30 06:36:24 +0100

    chore(eslint): enforce specific rules for markdown files.
    - Added `eslint-plugin-markdownlint` dev dependency to allow eslint to run linting rules for markdown. The plugin runs rules via the `markdownlint` package.
    
    - Added new config object for Markdown files, and specific rules to lint, (eg. enforce italics to be styled with a single asterisk instead of underscore, and increment ordered lists correctly, etc.).

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import markdownlintPlugin from 'eslint-plugin-markdownlint';
+import markdownlintPluginParser from 'eslint-plugin-markdownlint/parser.js';
 import prettier from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import path from 'node:path';
@@ -43,6 +45,38 @@ export default defineConfig([
           usePrettierrc: true,
         },
       ],
+    },
+  },
+
+  // Markdown files.
+  // The eslint markdownlint plugin (https://github.com/paweldrozd/eslint-plugin-markdownlint)
+  // runs markdownlint rules (https://github.com/DavidAnson/markdownlint/blob/main/doc/Rules.md).
+  {
+    files: ['**/*.md'],
+
+    plugins: { markdownlint: markdownlintPlugin },
+    languageOptions: { parser: markdownlintPluginParser },
+    rules: {
+      // Enforce a space inbetween a heading hash and the heading text (MD018)
+      'markdownlint/md018': 'error',
+      // Enforce that headings don't have multiple spaces between the hash and the heading text (MD019)
+      'markdownlint/md019': 'error',
+      // Enforce that each heading must be unique - no duplicate headings (MD024)
+      'markdownlint/md024': 'error',
+      // Enforce that ordered lists increment correctly (MD029)
+      'markdownlint/md029': ['error', { style: 'ordered' }],
+      // Enforce a space between a list marker (e.g. '-', '1.') and the list item text (MD030)
+      'markdownlint/md030': 'error',
+      // Enforce that code blocks use the fenced style instead of indented style (MD046)
+      'markdownlint/md046': ['error', { style: 'fenced' }],
+      // Enforce code block fences to use backticks (MD048)
+      'markdownlint/md048': ['error', { style: 'backtick' }],
+      // Enforce single asterisks for emphasis (italics) (MD049)
+      'markdownlint/md049': ['error', { style: 'asterisk' }],
+      // Enforce double asterisks for strong emphasis (bold) (MD050)
+      'markdownlint/md050': ['error', { style: 'asterisk' }],
+      // Enforce that link text should be descriptive (prevents "click here", "here", etc.) (MD059)
+      'markdownlint/md059': 'error',
     },
   },
 ]);
